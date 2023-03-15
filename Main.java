882190f9fe513df04793d584de1fe84823e30915
John Doe <mangalkunchalwar9999@gmail.com>
2023-03-15 22:59:53 +0530

    Welcome to the Snake & Ladder Problem

---
+++ Main.java
@@ -0,0 +1,7 @@
+package day2.snake_and_ladder_problem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake & Ladder Problem");
+    }
+}
